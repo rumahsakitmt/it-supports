7bd9031d8e0d51cb86a8fd3fb7f28440c6b9be5f
putuhema <putuhendramahendra@gmail.com>
2025-05-01 21:32:46 +0800

    feat: add typewriter effect by fancy

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,23 @@
 import SearchBar from "@/components/searchbar";
+import Typewriter from "@/fancy/components/text/typewriter";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center h-screen w-full  justify-center">
       <div className="max-w-xl space-y-2">
         <div>
-          <h1 className="text-base md:text-3xl font-bold">Cari <span className="italic">Masalah</span> (IT)</h1>
+          <h1 className="text-base md:text-3xl font-bold"> <span>Cari </span>
+            <Typewriter
+              text={[
+                "Masalahnya",
+                "Solusinya",
+              ]}
+              speed={100}
+              className="text-primary"
+              waitTime={1500}
+              deleteSpeed={40}
+              cursorChar={"|"} />
+          </h1>
           <p className="text-sm text-muted-foreground  md:text-base">Kurasi masalah-masalah IT RSUD Mamuju Tengah dan Solusinya.</p>
         </div>
         <SearchBar />
